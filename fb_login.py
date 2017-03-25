820448c429d764813d2583aff95ea4fe9abefd0e
NIshanth Vydana <nishanthvydanav@gmail.com>
2017-03-25 09:08:49 +0530

    Update fb_login.py

--- fb_login.py
+++ fb_login.py
@@ -5,10 +5,9 @@ browser = webdriver.Firefox(executable_path='D:\Users\Nishanth18\Desktop\geckodr
 browser.get("https://www.facebook.com/")
 time.sleep(10)
 username = browser.find_element_by_css_selector('#email')
-username.send_keys('nishanthvydanav@gmail.com')
+username.send_keys('**********')
 password = browser.find_element_by_css_selector('#pass')
-password.send_keys('v_nishanth18')
+password.send_keys('******')
 login = browser.find_element_by_css_selector('#u_0_q')
 login.click()
 
-D:\Users\Nishanth18\PycharmProjects\Selenium\facebook.py
